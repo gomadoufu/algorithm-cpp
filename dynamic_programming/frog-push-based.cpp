1d4dfba26d5578d26bbff93b57a546fe39e22544
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-05-21 01:21:22 +0900

    第5章終了

--- dynamic_programming/frog-push-based.cpp
+++ dynamic_programming/frog-push-based.cpp
@@ -5,47 +5,48 @@
 #include <vector>
 using namespace std;
 
-template <class T>
-void chmin(T& a, T b) {
-    if (a > b) {
-        a = b;
-    }
+template <class T> void chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+  }
 }
 
-const long long INF = 1LL << 60;
+const int64_t INF = 1LL << 60;
 
-int main() {
-    // 入力
-    int N;
-    cin >> N;
-    vector<long long> h(N);
-    for (int i = 0; i < N; ++i) cin >> h[i];
+auto main() -> int {
+  // 入力
+  int N;
+  cin >> N;
+  vector<int64_t> h(N);
+  for (int i = 0; i < N; ++i) {
+    cin >> h[i];
+  }
 
-    // 初期化 (最小化問題なので、INF に初期化)
-    vector<long long> dp(N, INF);
+  // 初期化 (最小化問題なので、INF に初期化)
+  vector<int64_t> dp(N, INF);
 
-    // 初期条件
-    dp[0] = 0;
+  // 初期条件
+  dp[0] = 0;
 
-    // ループ
-    // pull-based
-    // for (int i = 1; i < N; ++i) {
-    //     chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
-    //     if (i > 1) {
-    //         chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
-    //     }
-    // }
+  // ループ
+  // pull-based
+  // for (int i = 1; i < N; ++i) {
+  //     chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
+  //     if (i > 1) {
+  //         chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
+  //     }
+  // }
 
-    // push-based
-    for (int i = 0; i < N; ++i) {
-        if (i + 1 < N) {
-            chmin(dp[i + 1], dp[i] + abs(h[i] - h[i + 1]));
-        }
-        if (i + 2 < N) {
-            chmin(dp[i + 2], dp[i] + abs(h[i] - h[i + 2]));
-        }
+  // push-based
+  for (int i = 0; i < N; ++i) {
+    if (i + 1 < N) {
+      chmin(dp[i + 1], dp[i] + abs(h[i] - h[i + 1]));
     }
+    if (i + 2 < N) {
+      chmin(dp[i + 2], dp[i] + abs(h[i] - h[i + 2]));
+    }
+  }
 
-    // 答え
-    cout << dp[N - 1] << endl;
+  // 答え
+  cout << dp[N - 1] << endl;
 }
